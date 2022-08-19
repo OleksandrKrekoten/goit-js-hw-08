d39f0054400edb7e69b2a1b9c4491d05e168fddb
Oleksandr <alexkrekoten11@gmail.com>
2022-08-19 15:39:40 +0300

    /

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,12 @@
 import SimpleLightbox from "simplelightbox";
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
+import '../css/common.css';
+import '../css/01-gallery.css';
+
+import '../css/common.css';
+import '../css/03-feedback.css';
 
 
 
