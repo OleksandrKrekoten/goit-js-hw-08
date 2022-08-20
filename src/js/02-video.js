8eea7ff72fd2da3c81b2d8299317297ec15c4fc3
Oleksandr <alexkrekoten11@gmail.com>
2022-08-20 16:36:51 +0300

    /

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,32 +6,14 @@ const iframe = document.querySelector('iframe')
 
  const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
 
 const onPlay = function(data) {
-    // data is an object containing properties specific to that event
-    localStorage.setItem(TIME_KEY, JSON.stringify(data))
+    localStorage.setItem(TIME_KEY, JSON.stringify(data.seconds))
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 const settingsTime = JSON.parse(localStorage.getItem(TIME_KEY));
-console.log(settingsTime);
-player.setCurrentTime(settingsTime.seconds).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+player.setCurrentTime(settingsTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+})
